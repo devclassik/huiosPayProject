b48785f5ab4fca20151ac32c55233e678f1a38b9
Opemipo <alomajaopemipo8@gmail.com>
2023-03-13 12:21:15 +0100

    index on main: 880caa1 fix: landing page

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,14 +7,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-// isDisable: boolean = false;
+isSubmenu: boolean = false;
+
+ // Declare a variable to store the value of the switch case statement  
+ public result: string = 'H';  //H - hide
 
   constructor() { }
 
   clickHandler(){
-    console.log('i worked');
+    console.log('click effect');
     
   }
+
+
+  // Create a function to handle the switch case statement  
+  public switchCase(value: number): void {
+
+    console.log('from toggle', value);
+    
+    // Switch statement with cases  
+    switch (value) {
+
+      // Case 1  
+      case 1: {
+
+        this.result = 'Case 1';
+        console.log('result', this.result);
+        
+
+        break;
+
+      }
+
+      // Case 2  
+      case 2: {
+
+        this.result = 'Case 2';
+        console.log('result', this.result);
+
+
+        break;
+
+      }
+
+       // Case 3  
+       case 3: {
+
+        this.result = 'Case 3';
+        console.log('result', this.result);
+
+
+        break;
+
+      }
+
+      // Default Case  
+      default: {
+
+        this.result = 'Default Case';
+        console.log('result', this.result);
+
+
+        break;
+
+      }
+
+    }    
+
+  }  
 
 
   ngOnInit(): void {
